f8a16141e7fe0323ea0bbbf7cf98f2e65ee554bd
mike-port <miike.day@gmail.com>
2021-04-01 11:06:59 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submit', (e) => {
+  let messages = []
+  if (username.value === '' || username.value == null) {
+    messages.push('Name is required')
+  }
+
+  if (username.value.trim() == "" || password.value.trim() == "") {
+    alert("No blank values allowed, such as the spacebar.");
+    return false;
+  }
+
+  if (password.value.length <= 6) {
+    alert('Password must be longer than 6 characters')
+  }
+
+  if (password.value.length >= 20) {
+    messages.push('Password must be less than 20 characters')
+  }
+
+  if (password.value === 'password') {
+    messages.push('Password cannot be password')
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+  }
+})
